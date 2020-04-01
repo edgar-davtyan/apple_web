eaa006ff8a1cefbfb03773bf6071b8af11444a89
Edgar <edgar.davtyan.02@gmail.com>
2020-04-01 16:23:55 +0400

    added animations

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let product = document.querySelector(".products-box");
 let servicec = document.querySelector(".servicec-container");
 let product2 = document.querySelector(".products-box2");
+let nav = document.querySelector(".nav-content");
 window.onscroll = function f() {
+    if (document.documentElement.scrollTop > 670){
+        nav.classList.add("back")
+    }else {
+        nav.classList.remove("back")
+    }
     if (document.documentElement.scrollTop > 150) {
         product.classList.add("active")
     }if (document.documentElement.scrollTop > 650) {
