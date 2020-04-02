4de8c7143e5a9a30f652d9ecb56d0174613dbbcb
Edgar <edgar.davtyan.02@gmail.com>
2020-04-02 18:46:27 +0400

    added animation

--- script.js
+++ script.js
@@ -3,16 +3,18 @@ let servicec = document.querySelector(".servicec-container");
 let product2 = document.querySelector(".products-box2");
 let nav = document.querySelector(".nav-content");
 window.onscroll = function f() {
-    if (document.documentElement.scrollTop > 670){
+    if (document.documentElement.scrollTop > 670) {
         nav.classList.add("back")
-    }else {
+    } else {
         nav.classList.remove("back")
     }
     if (document.documentElement.scrollTop > 150) {
-        product.classList.add("active")
-    }if (document.documentElement.scrollTop > 650) {
-        servicec.classList.add("active2")
-    }if (document.documentElement.scrollTop > 1550){
-        product2.classList.add("active3")
+    product.classList.add("active")
     }
 };
+    if (document.documentElement.scrollTop > 650) {
+        servicec.classList.add("active2");
+    if (document.documentElement.scrollTop > 1550) {
+        product2.classList.add("active3")
+    }
+}
